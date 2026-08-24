87447bbecbf796968e73a32c94927ba629bad281
lucasdelacale <corp.lucasmartins@gmail.com>
2026-08-24 00:18:28 -0300

    - Update: Python - Aula 4

---
+++ Class/class-4.py
@@ -0,0 +1,8 @@
+# Python3 - Aula 4
+
+# Exercício de variaveis booleanas
+
+print(10 == 10) # Retorna True, pois 10 é igual a 10
+print (10 == 11) #Retorna False, pois 10 não é igual a 11
+print(type(10==10)) # Retorna <class 'bool'>, pois o resultado da comparação é um valor booleano.
+
