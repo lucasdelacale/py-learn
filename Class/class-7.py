d3ef63b4260ca44e48c7e115d9510fc3ad90981a
lucasdelacale <corp.lucasmartins@gmail.com>
2026-08-24 01:26:13 -0300

    - Update: Python - Aula 7 - Operadores aritméticos em Python
    - Explicação

---
+++ Class/class-7.py
@@ -0,0 +1,18 @@
+# Python - Aula 7
+
+"""
+Operadores aritméticos em Python
+- Soma: +
+- Subtração: -
+- Multiplicação: *
+- Divisão: /
+- Divisão inteira: //
+- Módulo: %
+- Exponenciação: **
+
+Ordem de precedência dos operadores aritméticos:
+1. Parênteses ()
+2. Exponenciação **
+3. Multiplicação * e Divisão / e Divisão inteira // e Módulo
+4. Adição + e Subtração -
+"""
---
+++ Notes/class-1.md
@@ -0,0 +1,6 @@
+
+# Python3
+
+## Print
+- A função print é utilizada para imprimir um argumento na tela
+- O modo de utilização correto é
