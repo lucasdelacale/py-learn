f5373b576713f8093e3057f8c2398721ab326953
lucasdelacale <corp.lucasmartins@gmail.com>
2026-08-24 00:17:02 -0300

    - Update: Python - Aula 2

---
+++ Class/class-2.py
@@ -0,0 +1,24 @@
+# Python3 - Aula 2
+
+"""
+Tipagem de Variáveis em Python: dinâmica e forte
+
+str = "Olá, Mundo!" - Variável do tipo string
+int = 10 - Variável do tipo inteiro
+float = 10.5 - Variável do tipo float
+bool = True ou False - Variável do tipo booleano
+
+Aspas duplas (" ") ou simples (' ') 
+podem ser utilizadas para definir strings em Python.
+
+Escape de caracteres: 
+\n (quebra de linha)
+\t (tabulação)
+\\ (barra invertida)
+\' (aspas simples)
+\" (aspas duplas)
+
+O escape de caracteres é utilizado para que a linguagem ignore o próximo caractere.
+
+r (raw string) - Utilizado para que a linguagem ignore os escapes de caracteres.
+"""
