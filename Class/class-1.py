96be6ad8073269bb022d4222fc20335398d7e79e
lucasdelacale <corp.lucasmartins@gmail.com>
2026-08-24 00:03:28 -0300

    - Update: Python - Aula 1

---
+++ Class/class-1.py
@@ -0,0 +1,10 @@
+# Python3 - Aula 1
+
+# Função print() - Imprime na tela
+print("Olá, Mundo!")  # Imprime a mensagem "Olá, Mundo!" na tela
+
+# Passando mais de um argumento para a função print()
+print("Dois", "argumentos")  # Imprime a mensagem "Dois argumentos" na tela
+
+# Utilizando a função sep para definir o separador entre os argumentos
+print("Bem", "Vindo!", sep="-")  # Imprime a mensagem "Bem-Vindo!" na tela
