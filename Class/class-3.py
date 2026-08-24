e8f9678dbcd66b7fe39afd0c9f7e40743990017f
lucasdelacale <corp.lucasmartins@gmail.com>
2026-08-24 00:18:17 -0300

    - Update: Python - Aula 3

---
+++ Class/class-3.py
@@ -0,0 +1,30 @@
+# Python3 - Aula 3
+
+"""
+str = "Olá, Mundo!" - Variável do tipo string
+int = 10 - Variável do tipo inteiro
+float = 10.5 - Variável do tipo float
+bool = True ou False - Variável do tipo booleano
+"""
+
+str = "Olá, Mundo!"  # Variável do tipo string
+int = 10  # Variável do tipo inteiro
+float = 10.5  # Variável do tipo float
+bool = True or False  # Variável do tipo booleano
+
+print(str)  # Imprime a variável str na tela
+print(int)  # Imprime a variável int na tela
+print(float)  # Imprime a variável float na tela
+print(bool)  # Imprime a variável bool na tela
+
+# Função type() - Retorna o tipo da variável
+print(type(str))  # Retorna o tipo da variável str
+print(type(int))  # Retorna o tipo da variável int
+print(type(float))  # Retorna o tipo da variável float
+print(type(bool))  # Retorna o tipo da variável bool
+
+# Passando mais de um argumento para a função print() e type()
+print("Tipagem de dados", "\n", type(str), type(int))  # Retorna o tipo das variáveis str e int
+print("Tipagem de dados", "\n", type(float), type(bool))  # Retorna o tipo das variáveis float e bool
+
+
