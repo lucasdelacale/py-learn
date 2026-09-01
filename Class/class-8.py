1e097a0bfb0a2b1fca223f8ced5be5403a688869
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-01 12:36:08 -0300

    - Update: Python - Aula 8 - Concatenação - Repetição de strings

---
+++ Class/class-8.py
@@ -0,0 +1,13 @@
+# Python - Aula 8
+
+"""
+Concatenação (+) e Repetição (*) de Strings
+- Concatenação: +
+- Repetição: * 
+"""
+
+concatenacao = "A" + "B" + "C"
+repeticao = "A" * 5
+
+print ("Concatenação:", concatenacao)
+print ("Repetição:", repeticao)
