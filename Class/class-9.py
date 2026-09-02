82641c6db08627ff622c5d31a64cc76f8af64e91
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-02 08:17:55 -0300

    - Update: Python - Aula 9 - Precedência de operadores

---
+++ Class/class-9.py
@@ -0,0 +1,21 @@
+# Python - Aula 9
+
+"""
+Precedência de operadores
+1. ()
+2. **
+4. * / // %
+5. + -
+"""
+
+conta_1 = 1 + 1 ** 5 + 5
+
+calculo_1 = 1 ** 5
+calculo_2 = calculo_1 + 5
+calculo_3 = calculo_2 + 1
+
+print ("Resultado do calculo 1:", calculo_1)
+print ("Resultado do calculo 2:", calculo_2)
+print ("Resultado do calculo 3:", calculo_3)
+
+print ("Resultado da final:", conta_1)
