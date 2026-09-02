c9a240cd48c79aa7a44d2d5395fe1585a0076082
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-02 08:18:49 -0300

    - Update: Python - Exercício 1 - Cálculo de IMC

---
+++ Class/ex-1.py
@@ -0,0 +1,13 @@
+# Execício - Calculo de IMC
+
+nome = "Lucas Martins"
+altura = 1.80
+peso = 60
+imc = peso / (altura * altura)
+
+print(f"{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc}")
+
+"""
+Na aula o professor ainda não havia ensinado f strings,
+como eu ja sei utilizar, o exercício foi feito dessa forma.
+"""
