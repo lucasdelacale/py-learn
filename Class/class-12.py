749cb68a9064405cdd8b3895aa4fb258baa27d54
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-04 06:07:31 -0300

    - Update: Python - Aula 12 - Função input (para coleta de dados)

---
+++ Class/class-12.py
@@ -0,0 +1,13 @@
+# Python - Aula 12
+
+"""
+Coleta de dados do usuário utilizando a função input()
+e transformar esse input em uma variável
+
+**Sempre em string, em casos de números é necessário utilizar a formatação
+"""
+
+numero1 = input("Digite um número: ")
+numero2 = input("Digite outro número: ")
+
+print(f"A soma dos dois números é: {numero1 + numero2}")
