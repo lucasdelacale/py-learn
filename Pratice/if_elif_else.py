12327d59aed533539503ce2fe95d724779534c75
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-05 16:10:02 -0300

    - Prática: if elif else - Sem consultar código - Apenas com a memória da ultuma aula estudada - Momento aleatório do dia para estudo de memorização e exploração de código.

---
+++ Pratice/if_elif_else.py
@@ -0,0 +1,20 @@
+# Prática de memorização
+
+# Um bloco de código condicional com if, elif e else.
+# Sem consultar códigos anteriores, exercício de memorização e exploração.
+
+# Ideia: alteração e verificação de senha
+
+print("Para escolher uma senha, ela deve conter no mínimo 10 caracteres.")
+senha_1 = input("Digite a nova senha: ")
+senha_2 = input("Digite novamente: ")
+
+if senha_1 == senha_2:
+    print("A sua senha foi redefinida!")
+
+elif senha_1 != senha_2:
+    print("As duas senhas não coincidem, tente novamente.")
+
+
+# Essa tela, contém alguns erros, no entanto, ela foi feita apenas com o conteúdo de minha memória,
+# sem consultar nenhum código anteriore ou aula, utilizando apenas o contexto do curso estudado atualmente.
