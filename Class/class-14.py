aae4e056e3460f8c4cfc27f6f530fc7ffbcf4261
lucasdelacale <corp.lucasmartins@gmail.com>
2026-09-13 01:02:17 -0300

    - Update: Python - Aula 14 - Operadores de comparação (relacionais)

---
+++ Class/class-14.py
@@ -0,0 +1,26 @@
+# Operadores de comparação (relacionais)
+"""
+OP      Significado     Exemplo(true)
+>       maior           2 > 1
+>=      maior ou igual  2 >= 2
+<       menor           1 < 2
+<=      menor ou igual  2 <= 2
+==      igual           "a" == "a"
+!=      diferente       "a" != "b"
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = "a" == "a"
+diferente = "a" != "b"
+
+print("Exemplos de operadores de comparação (relacionais)")
+print("Exemplos True")
+
+print(f"Maior: {maior}")
+print (f"Maior ou igual: {maior_ou_igual}")
+print(f"Menor: {menor}")
+print(f"Menor ou igual: {menor_ou_igual}")
+print(f"Igual: {igual}")
+print(f"Diferente: {diferente}")
